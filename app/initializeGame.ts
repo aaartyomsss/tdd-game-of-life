d465af3dcdd804f79d38e118c8a3f33522f288d4
Artjom Savin <artjom.savin@nordhealth.com>
2024-05-11 09:52:02 +0300

    Add error handling

--- app/initializeGame.ts
+++ app/initializeGame.ts
@@ -1,8 +1,15 @@
 import { GameOfLife } from './GameOfLife'
 
 export const initializeGame = (path: string) => {
-  const file = GameOfLife.readEncodingFile(path)
-  const { x, y, encoding } = GameOfLife.parseFile(file)
-  const game = new GameOfLife(x, y, encoding)
-  return game
+  try {
+    const file = GameOfLife.readEncodingFile(path)
+    const { x, y, encoding } = GameOfLife.parseFile(file)
+    if (!x) throw new Error('x not found in file')
+    if (!y) throw new Error('y not found in file')
+    if (!encoding) throw new Error('encoding not found')
+    const game = new GameOfLife(x, y, encoding)
+    return game
+  } catch (error) {
+    throw error
+  }
 }
--- test/entrypoint.test.ts
+++ test/entrypoint.test.ts
@@ -9,9 +9,21 @@ const dir = './testable/tmp'
 const smallGliderPath = dir + '/glider.rle'
 const gliderString = '#C This is a glider.\nx = 3, y = 3\nbo$2bo$3o!'
 
+const corruptX = '#C This is a glider.\nb = 3, y = 3\nbo$2bo$3o!'
+const corruptXPath = dir + '/x_corrupt_glider.rle'
+
+const corruptY = '#C This is a glider.\nx = 3, v \nbo$2bo$3o!'
+const corruptYPath = dir + '/y_corrupt_glider.rle'
+
+const corruptEncoding = '#C This is a glider.\nx = 3, y = 3 \n'
+const corruptEncodingPath = dir + '/encoding_corrupt_glider.rle'
+
 beforeAll(async () => {
   fs.mkdirSync(dir, { recursive: true })
   await writeFile(smallGliderPath, gliderString)
+  await writeFile(corruptXPath, corruptX)
+  await writeFile(corruptYPath, corruptY)
+  await writeFile(corruptEncodingPath, corruptEncoding)
 })
 
 afterAll(() => {
@@ -50,5 +62,23 @@ describe('Tests function which sets up the game ', () => {
       bbo
       ooo
     `)
+  })
+
+  test('Expect to throw error in case of wrong file', () => {
+    vex(() => initializeGame('./sasmhjbdkjashdjas')).toThrowError(/file/)
+  })
+
+  test('Expect to throw error in case x was not provided in file ', () => {
+    vex(() => initializeGame(corruptXPath)).toThrowError(/x not found/)
+  })
+
+  test('Expect to throw error in case y was not provided in file ', () => {
+    vex(() => initializeGame(corruptYPath)).toThrowError(/y not found/)
+  })
+
+  test('Expect to throw error in case encoding was not provided in file ', () => {
+    vex(() => initializeGame(corruptEncodingPath)).toThrowError(
+      /encoding not found/
+    )
   })
 })
