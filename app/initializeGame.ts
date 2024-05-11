2ad76db3853a81c06398cdd9978994a6d3803643
Artjom Savin <artjom.savin@nordhealth.com>
2024-05-11 09:55:00 +0300

    Handle errors in own function

--- app/initializeGame.ts
+++ app/initializeGame.ts
@@ -3,13 +3,26 @@ import { GameOfLife } from './GameOfLife'
 export const initializeGame = (path: string) => {
   try {
     const file = GameOfLife.readEncodingFile(path)
-    const { x, y, encoding } = GameOfLife.parseFile(file)
-    if (!x) throw new Error('x not found in file')
-    if (!y) throw new Error('y not found in file')
-    if (!encoding) throw new Error('encoding not found')
+    const input = GameOfLife.parseFile(file)
+    const { x, y, encoding } = handleErrors(input)
     const game = new GameOfLife(x, y, encoding)
     return game
   } catch (error) {
     throw error
   }
+}
+
+type GameInput = {
+  x?: number | undefined
+  y?: number | undefined
+  encoding: string
+}
+
+const handleErrors = (input: GameInput) => {
+  const { x, y, encoding } = input
+  if (!x) throw new Error('x not found in file')
+  if (!y) throw new Error('y not found in file')
+  if (!encoding) throw new Error('encoding not found')
+
+  return { x, y, encoding }
 }
